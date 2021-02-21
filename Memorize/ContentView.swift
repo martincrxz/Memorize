d14bc8747ad717691945a08e9d257a7dab0ec57e
martin <martincruzgarcia93@gmail.com>
2021-02-21 17:25:46 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Martín García on 21/02/2021.
+//  Copyright © 2021 Martín García. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
